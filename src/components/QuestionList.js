69eeb3b68ba162f39f61b4be1fc23edfab96c370
terence <terenceshawn18@outlook.com>
2023-11-20 15:14:13 +0300

    complete

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,24 +1,49 @@
-import React,{useState,useEffect} from "react"
-import QuestionItem from './QuestionItem'
-import QuestionForm from "./QuestionForm"
+import React, { useState, useEffect } from 'react';
+import QuestionItem from './QuestionItem';
+
 function QuestionList() {
-  const[questions,setQuestion]=useState([])
-  const[form,setForm]=useState(false)
-  useEffect(()=>{
+  const [questions, setQuestions] = useState([]);
+
+  useEffect(() => {
+    // Fetch questions when the component mounts
     fetch('http://localhost:4000/questions')
-     .then(r=>r.json())
-     .then((questions)=>setQuestion(questions))
-  },[])
-  function handleAdd(newQuestion){
-    setQuestion([...questions,newQuestion])
-  }
+      .then((response) => response.json())
+      .then((data) => setQuestions(data))
+      .catch((error) => console.error('Error fetching questions: ', error));
+  }, []);
+
+  const handleCorrectAnswerChange = (id, newCorrectIndex) => {
+    const updatedQuestions = questions.map((question) => {
+      if (question.id === id) {
+        return { ...question, correctIndex: newCorrectIndex };
+      }
+      return question;
+    });
+
+    setQuestions(updatedQuestions);
+
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ correctIndex: newCorrectIndex }),
+    }).catch((error) =>
+      console.error('Error updating correct answer on the server: ', error)
+    );
+  };
+
   return (
     <section>
-      <button onClick={()=>setForm(!form)}>ask Question</button>
-      {form && <QuestionForm onSubmit={handleAdd} />}
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}
-        {questions.map((item)=> <QuestionItem key={item.id} question={item} />)}
+      <ul>
+        {questions.map((question) => (
+          <QuestionItem
+            key={question.id}
+            question={question}
+            onCorrectAnswerChange={handleCorrectAnswerChange}
+          />
+        ))}
       </ul>
     </section>
   );
